b573b37075625b7770df12d8c7fa54856c175cad
Serge Svalov <wndrsw@gmail.com>
2026-07-21 16:30:21 +0300

    fix: explicit host for vite in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run dev -- --host 127.0.0.1',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: true,
   },
